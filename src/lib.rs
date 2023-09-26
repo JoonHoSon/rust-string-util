96970631820e50fc485b9fc9a2dfe09a8d9a9265
JoonHo Son <joonho.son@me.com>
2023-09-26 14:50:19 +0900

    refactor: 자동생성 코드 삭제

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,1 @@
-mod string_util;
-
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+pub mod string_util;
