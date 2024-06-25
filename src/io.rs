89872ac92ca0c80eb3317e7861be7729cdbbd8e8
JoonHo Son <joonho.son@me.com>
2024-06-25 18:32:34 +0900

    feat: io 관련 함수 작성 중
    테스트 때문에 주석 처리함
    
    #3

--- src/io.rs
+++ src/io.rs
@@ -24,44 +24,44 @@ pub enum DirectoryDateType {
     YYYY
 }
 
-pub fn generate_path(parent_path: &Path, date_type: DirectoryDateType, separator: Option<&str>) -> Result<Box<Path>, InvalidArgumentError> {
-    // check exist parent path
-    if parent_path.exists().not() {
-        let path_str = parent_path.as_os_str();
-        let message = format!("[{:?}] 경로가 존재하지 않습니다.", path_str);
-
-        return Err(InvalidArgumentError::new(message.as_str()));
-    }
-
-    let now = chrono::Local::now();
-
-    let mut path: Vec<&str> = vec![];
-
-    // yyyy
-    path.push(now.year().to_string().as_str());
-
-    if date_type != DirectoryDateType::YYYY {
-        path.push(format!("{:0>2}", now.month().to_string()).as_str());
-    }
-
-    if date_type == DirectoryDateType::YYYYMMDD {
-        path.push(format!("{:0>2}", now.day().to_string()).as_str());
-    }
-
-    let mut result = PathBuf::from(parent_path);
-
-    for dir in path {
-        result = result.join(dir);
-    }
-
-    if !&result.exists() {
-        let created_result = std::fs::create_dir_all(result.as_ref());
-
-        if created_result.is_err() {
-            // TODO(joonho): 2024-06-24 create_dir_all에서 반환되는 에러 확인
-            return Err(InvalidArgumentError::new("Failed to create directory"))
-        }
-    }
-
-    return  Ok(result.into_boxed_path());
-}
+// pub fn generate_path(parent_path: &Path, date_type: DirectoryDateType, separator: Option<&str>) -> Result<Box<Path>, InvalidArgumentError> {
+//     // check exist parent path
+//     if parent_path.exists().not() {
+//         let path_str = parent_path.as_os_str();
+//         let message = format!("[{:?}] 경로가 존재하지 않습니다.", path_str);
+//
+//         return Err(InvalidArgumentError::new(message.as_str()));
+//     }
+//
+//     let now = chrono::Local::now();
+//
+//     let mut path: Vec<&str> = vec![];
+//
+//     // yyyy
+//     path.push(now.year().to_string().as_str());
+//
+//     if date_type != DirectoryDateType::YYYY {
+//         path.push(format!("{:0>2}", now.month().to_string()).as_str());
+//     }
+//
+//     if date_type == DirectoryDateType::YYYYMMDD {
+//         path.push(format!("{:0>2}", now.day().to_string()).as_str());
+//     }
+//
+//     let mut result = PathBuf::from(parent_path);
+//
+//     for dir in path {
+//         result = result.join(dir);
+//     }
+//
+//     if !&result.exists() {
+//         let created_result = std::fs::create_dir_all(result.as_ref());
+//
+//         if created_result.is_err() {
+//             // TODO(joonho): 2024-06-24 create_dir_all에서 반환되는 에러 확인
+//             return Err(InvalidArgumentError::new("Failed to create directory"))
+//         }
+//     }
+//
+//     return  Ok(result.into_boxed_path());
+// }
